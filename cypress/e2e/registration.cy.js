51f5bccbac1db57135be18e4f29eb2f9213539cf
Oxana2332 <far_north@ro.ru>
2025-03-23 18:48:24 +0300

    начало работы с cypress и первый тест - registration

---
+++ cypress/e2e/registration.cy.js
@@ -0,0 +1,17 @@
+describe('Test registration', function () {
+	const NAME = 'Oxana';
+	const PHONE = '(911)718-15-75';
+	const EMAIL = 'far7north@gmail.com';
+
+	it('Test registration positive', function () {
+		cy.visit('https://kotofoto.ru/');
+		cy.get('.icon-user').should('be.visible').click();
+		cy.contains('Регистрация').click();
+
+		cy.get('#regname').type(NAME).should('have.value', NAME);
+		cy.get('#regtell').type(PHONE).should('have.value', `+7${PHONE}`);
+		cy.get('#regmail').type(EMAIL).should('have.value', EMAIL);
+
+		cy.get('#registerkoto').should('be.visible').click();
+	});
+});
