fe6dc9b537e93ba4326989c73e2274c1f3940807
Oxana2332 <far_north@ro.ru>
2025-03-23 20:02:21 +0300

    тест авторизации

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,11 @@
+describe('Log in', () => {
+	it('Sign in', () => {
+		cy.visit('https://kotofoto.ru/');
+		cy.get('.icon-user').should('be.visible').click();
+
+		cy.get('#loginmail').type('far_north@ro.ru');
+		cy.get('#loginpass').type('R8c5d7b');
+
+		cy.get('#loginkotofoto').should('be.visible').click();
+	});
+});
